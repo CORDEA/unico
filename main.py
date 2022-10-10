d5bde14e9769a003cd045e891f62c10c4e2f550e
Yoshihiro Tanaka <contact@cordea.jp>
2022-10-10 14:12:11 +0900

    display the text as hex utf-8

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import argparse
 
 
+def utf8(text):
+    return ' '.join([hex(r) for r in bytes(text, 'utf-8')])
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--utf32', action='store_true')
@@ -8,6 +12,7 @@ def main():
     parser.add_argument('--utf8', action='store_true')
     parser.add_argument('text')
     args = parser.parse_args()
+    print(utf8(args.text))
 
 
 if __name__ == '__main__':
