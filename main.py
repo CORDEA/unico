58a134f493aad78da4cad6ea4e6f911d1bab74de
Yoshihiro Tanaka <contact@cordea.jp>
2022-10-10 14:44:57 +0900

    support binary format

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import argparse
+from enum import Enum
+from textwrap import wrap
+
+
+class Format(Enum):
+    HEX = 'hex'
+    BINARY = 'binary'
+
+    def __str__(self):
+        return self.value
 
 
 class Printer:
-    def __init__(self, text, sep):
+    def __init__(self, text, sep, fmt):
         self.text = text
         self.sep = sep
+        self.fmt = fmt
 
     def utf8(self):
-        return self.sep.join([hex(r) for r in bytes(self.text, 'utf-8')])
+        return self.sep.join([self.format(r, 8) for r in bytes(self.text, 'utf-8')])
 
     def utf16(self):
         b = iter(bytes(self.text.encode('utf-16-be')))
-        return self.sep.join([hex(c[0] << 8 | c[1]) for c in zip(b, b)])
+        return self.sep.join([self.format(c[0] << 8 | c[1], 16) for c in zip(b, b)])
 
     def utf32(self):
-        return self.sep.join([hex(ord(c)) for c in self.text])
+        return self.sep.join([self.format(ord(c), 32) for c in self.text])
+
+    def format(self, query, digit):
+        if self.fmt == Format.HEX:
+            return hex(query)
+        return ' '.join(wrap(bin(query)[2:].zfill(digit), 4))
 
 
 def main():
@@ -23,9 +39,10 @@ def main():
     parser.add_argument('--utf16', action='store_true')
     parser.add_argument('--utf8', action='store_true')
     parser.add_argument('--sep', default=' ')
+    parser.add_argument('--format', type=Format, choices=Format, default=Format.HEX)
     parser.add_argument('text')
     args = parser.parse_args()
-    printer = Printer(args.text, args.sep)
+    printer = Printer(args.text, args.sep, args.format)
     if args.utf8:
         print(printer.utf8())
         return
