4738ccae2dc2a1797d2967c732cb77fce7f08253
Yoshihiro Tanaka <contact@cordea.jp>
2022-10-10 14:20:15 +0900

    allow specifying separator

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import argparse
 
 
-def utf8(text):
-    return ' '.join([hex(r) for r in bytes(text, 'utf-8')])
+class Printer:
+    def __init__(self, text, sep):
+        self.text = text
+        self.sep = sep
 
+    def utf8(self):
+        return self.sep.join([hex(r) for r in bytes(self.text, 'utf-8')])
 
-def utf16(text):
-    b = iter(bytes(text.encode('utf-16-be')))
-    return ' '.join([hex(c[0] << 8 | c[1]) for c in zip(b, b)])
+    def utf16(self):
+        b = iter(bytes(self.text.encode('utf-16-be')))
+        return self.sep.join([hex(c[0] << 8 | c[1]) for c in zip(b, b)])
 
-
-def utf32(text):
-    return ' '.join([hex(ord(c)) for c in text])
+    def utf32(self):
+        return self.sep.join([hex(ord(c)) for c in self.text])
 
 
 def main():
@@ -19,9 +22,11 @@ def main():
     parser.add_argument('--utf32', action='store_true')
     parser.add_argument('--utf16', action='store_true')
     parser.add_argument('--utf8', action='store_true')
+    parser.add_argument('--sep', default=' ')
     parser.add_argument('text')
     args = parser.parse_args()
-    print(utf16(args.text))
+    printer = Printer(args.text, args.sep)
+    print(printer.utf16())
 
 
 if __name__ == '__main__':
