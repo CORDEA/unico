119196ac4546b377428bc4de042b3facda5c7874
Yoshihiro Tanaka <contact@cordea.jp>
2022-10-10 14:16:32 +0900

    display the text as hex utf-16

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def utf8(text):
     return ' '.join([hex(r) for r in bytes(text, 'utf-8')])
 
 
+def utf16(text):
+    b = iter(bytes(text.encode('utf-16-be')))
+    return ' '.join([hex(c[0] << 8 | c[1]) for c in zip(b, b)])
+
+
 def utf32(text):
     return ' '.join([hex(ord(c)) for c in text])
 
@@ -16,7 +21,7 @@ def main():
     parser.add_argument('--utf8', action='store_true')
     parser.add_argument('text')
     args = parser.parse_args()
-    print(utf32(args.text))
+    print(utf16(args.text))
 
 
 if __name__ == '__main__':
