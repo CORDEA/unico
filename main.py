061b5239bc913ca620a0fb0c7e877965819f0a6f
Yoshihiro Tanaka <contact@cordea.jp>
2022-10-10 14:03:33 +0900

    parse command line args

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import argparse
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--utf32', action='store_true')
+    parser.add_argument('--utf16', action='store_true')
+    parser.add_argument('--utf8', action='store_true')
+    parser.add_argument('text')
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
